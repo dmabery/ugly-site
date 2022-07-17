42bc3c833441c9c65a461608b7ed56c2729f08ea
daltonmabery <maberydalton@gmail.com>
2022-07-17 10:14:46 -0700

    add meta to index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Meta from '../components/Meta'
 
 export default function Home() {
   return (
